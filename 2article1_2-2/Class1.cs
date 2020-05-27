f8adceda14ebc7be5b0dba3b6fa79677fd44c5a2
xzforever <xzforever.1234@gmail.com>
2020-05-28 00:46:08 +0300

    Добавьте файлы проекта.

---
+++ 2article1_2-2/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2article1_2_2
+{
+    class Electronics
+    {
+        public string Name;
+        public int Voltage;
+        public int Weight;
+
+        public void PrintElectro()
+        {
+            Console.WriteLine($"Type: {Name}");
+            Console.WriteLine($"Voltage: {Voltage}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine();
+        }
+    }
+    class Books
+    {
+        public string Name;
+        public int Weight;
+        public int PageNum;
+
+        public void PrintBooks()
+        {
+            Console.WriteLine($"Type: {Name}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Number of pages: {PageNum}");
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ 2article1_2-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2article1_2_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Electronics pc = new Electronics();
+            pc.Name = "Computer";
+            pc.Voltage = 12;
+            pc.Weight = 5;
+
+            Electronics phone = new Electronics();
+            phone.Name = "Phone";
+            phone.Voltage = 10;
+            phone.Weight = 03;
+
+            Books book1 = new Books();
+            book1.Name = "Novel";
+            book1.Weight = 05;
+            book1.PageNum = 399;
+
+            Books book2 = new Books();
+            book2.Name = "Horror";
+            book2.Weight = 04;
+            book2.PageNum = 150;
+
+            pc.PrintElectro();
+            phone.PrintElectro();
+            book1.PrintBooks();
+            book2.PrintBooks();
+
+        }
+    }
+}
